014a6d6fc5132536950d8f7aa05cad6be22a4f24
sauregens@gmail.com <OliveMoor@MacBook-Pro-Ira.lan>
2020-10-15 20:13:32 +0400

    step 5-6

--- Makefile
+++ Makefile
@@ -1,11 +1,17 @@
 install:
-	 npm install
+	npm install
 
 brain-games:
-	 node bin/brain-games.js
+	node bin/brain-games.js
+
+brain-even:
+	node games/brain-even
+
+brain-calc:
+	node games/brain-calc.js
 
 publish:
-	 npm publish --dry-run
+	npm publish --dry-run
 
 make lint:
 	npx eslint .
---
+++ src/index.js
@@ -0,0 +1,33 @@
+
+
+import promptly from 'promptly';
+
+export const getRandomNum = (min = 0, max = 100) => Math.floor(Math.random() * ((max - min) + 1)) + min;
+
+const startGame = async (description, generateData) => {
+
+  console.log('Welcome to the Brain Games!');
+
+  const name = await promptly.prompt('May I have your name? ');
+  console.log(`Hello, ${name}!`);
+  console.log(`${description}`);
+
+  for (let i = 0; i < 3; i += 1) {
+    const gameData = generateData();
+    const [question, answer] = gameData;
+
+    console.log(`Question: ${question}`);
+    const userAnswer = await promptly.prompt('Your answer: ');
+
+   if (userAnswer === answer) {
+      console.log('Correct!');
+    } else {
+      console.log(`"${userAnswer}" is wrong answer ;(. Correct answer was "${answer}".\nLet's try again, ${name}!`);
+      return;
+    }
+  }
+
+  console.log(`Congratulations, ${name}!`);
+};
+
+export default startGame;
