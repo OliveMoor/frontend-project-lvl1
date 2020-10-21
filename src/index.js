4844e878f75cf59591b8f68f17c8cce674e67ac5
sauregens@gmail.com <OliveMoor@MacBook-Pro-Ira.lan>
2020-10-21 15:51:07 +0400

    change eslint.yaml

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import promptly from 'promptly';
 
-export const getRandomNum = (min = 0, max = 100) => Math.floor(Math.random() * (max - min)) + min;
+export const getRandomNum = (min = 0, max = 100) => Math.floor(Math.random() * (max - min) + 1) + min;
 
 const startGame = async (description, generateData) => {
   console.log('Welcome to the Brain Games!');
